aba19bb574f553c1e187a65abf30a1ef019277f5
zmeyer44 <meyerz@wustl.edu>
2020-06-07 20:10:42 -0400

    vft

--- src/components/VirtualFieldtrip.js
+++ src/components/VirtualFieldtrip.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class VirtualFieldtrip extends Component {
-    render() {
-        return (
-            <div>
-                trips here
+  render() {
+    return (
+      <div className="container vft">
+        <div className="card">
+          <div className="front">
+            <img src="../../img/space.jpg" height="400px"></img>
+          </div>
+          <div className="back">
+            <div className="details">
+              <h2>Section Title</h2>
+              <div className="social-icons">
+                <ul>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  <li>
+                    <a href=""></a>
+                  </li>
+                  
+                </ul>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
