5ac3c4aa88395a128e896e09061cf3be8bc1c45e
zmeyer44 <meyerz@wustl.edu>
2020-06-17 14:52:45 -0400

    done

--- src/components/Reading.js
+++ src/components/Reading.js
@@ -9,9 +9,9 @@ export default class Reading extends Component {
             Reading & More
           </h1>
         </div>
-        <div className="row ">
-            <div className="SRG col-5">
-                <h2>Fluency Guides</h2>
+        <div className="row mx-auto">
+            <div className="SRG col-3">
+                <h2 className="m-3">Fluency Guides</h2>
             <ul className="text-capitalize">
                
                 <li>
@@ -25,6 +25,124 @@ export default class Reading extends Component {
                 </li>
                 <li>
                     <a href="..\fluency\5th Fluency.pdf" download>5th grade fluency</a>
+                </li>
+                <li>
+                    <a href="..\fluency\6th Fluency.pdf" download>6th grade fluency</a>
+                </li>
+            </ul>
+        </div>
+        <div className="SRG col-3">
+                <h2 className="m-3">Decodables</h2>
+            <ul className="text-capitalize">
+               
+                <li>
+                    <a href="..\fluency\K-Week 1 Decodables.pdf" download>K Week 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\K- Week 2 Decodables.pdf" download>K Week 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\K- Week 3 Decodables.pdf" download>K Week 3</a>
+                </li>
+                <li>
+                    <a href="..\fluency\1st Grade -Week 1 Decodables.pdf" download>1st Grade Week 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\_1st Grade -Week 2 Decodables.pdf" download>1st Grade Week 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\_1st Grade -Week 3 Decodables (1).pdf" download>1st Grade Week 3</a>
+                </li>
+                <li>
+                    <a href="..\fluency\2nd Grade -Week 1 Decodables.pdf" download>2nd Grade Week 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\2nd Grade -Week 2 Decodables.pdf" download>2nd Grade Week 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\2nd Grade -Week 3 Decodables (1).pdf" download>2nd Grade Week 3</a>
+                </li>
+                <li>
+                    <a href="..\fluency\3rd Grade -Week 1 Decodables.pdf" download>3rd Grade Week 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\3rd Grade -Week 2 Decodables.pdf" download>3rd Grade Week 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\3rd Grade -Week 3 Decodables (1).pdf" download>3rd Grade Week 3</a>
+                </li>
+                <li>
+                    <a href="..\fluency\4th Grade -Week 1 Decodables.pdf" download>4th Grade Week 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\4th Grade -Week 2 Decodables.pdf" download>4th Grade Week 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\4th Grade -Week 3 Decodables (1).pdf" download>4th Grade Week 3</a>
+                </li>
+                <li>
+                    <a href="..\fluency\5th Grade -Week 1 Decodables.pdf" download>5th Grade Week 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\5th Grade -Week 2 Decodables.pdf" download>5th Grade Week 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\5th Grade -Week 3 Decodables (1).pdf" download>5th Grade Week 3</a>
+                </li>
+            </ul>
+        </div>
+        <div className="SRG col-3">
+                <h2 className="m-3">Writing Choice Board</h2>
+            <ul className="text-capitalize">
+            <li>
+                    <a href="..\fluency\Kindergarten Writing Choice Board.pdf" download>Kindergarten Writing Choice Board</a>
+                </li>
+            <li>
+                    <a href="..\fluency\2nd Grade Writing Choice Board.pdf" download>1st grade Writing Choice Board</a>
+                </li>
+                <li>
+                    <a href="..\fluency\2nd Grade Writing Choice Board.pdf" download>2nd grade Writing Choice Board</a>
+                </li>
+                <li>
+                    <a href="..\fluency\3rd Grade Writing Choice Board.pdf" download>3rd grade Writing Choice Board</a>
+                </li>
+                <li>
+                    <a href="..\fluency\4th Grade Writing Choice Board.pdf" download>4th grade Writing Choice Board</a>
+                </li>
+                <li>
+                    <a href="..\fluency\5th Grade Writing Choice Board.pdf" download>5th grade Writing Choice Board</a>
+                </li>
+                <li>
+                    <a href="..\fluency\6th Grade Writing Choice Board.pdf" download>6th grade Writing Choice Board</a>
+                </li>
+            </ul>
+        </div>
+        <div className="SRG col-3">
+                <h2 className="m-3">Extra Activities</h2>
+            <ul className="text-capitalize">
+            <li>
+                    <a href="..\fluency\k1_f005_fluency_letter_wheel.pdf" download>Fluency Letter wheel</a>
+                </li>
+            <li>
+                    <a href="..\fluency\k1_f007_speedy_rime_words.pdf" download>speedy rime words</a>
+                </li>
+                <li>
+                    <a href="..\fluency\k1_f009_fast_match.pdf" download>fast match</a>
+                </li>
+                <li>
+                    <a href="..\fluency\k1_f010_fast_words.pdf" download>fast words</a>
+                </li>
+                <li>
+                    <a href="..\fluency\k1_f011_i_read_you_point.pdf" download>I read you point</a>
+                </li>
+                <li>
+                    <a href="..\fluency\k1_f013_speedy_phrases(1).pdf" download>Speedy phrases 1</a>
+                </li>
+                <li>
+                    <a href="..\fluency\k1_f013_speedy_phrases.pdf" download>Speedy phrases 2</a>
+                </li>
+                <li>
+                    <a href="..\fluency\k1_f021_readers_theater.pdf" download>readers theater</a>
                 </li>
             </ul>
         </div>
