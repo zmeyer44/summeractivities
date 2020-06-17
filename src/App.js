f0af6df4c0ad622b99f00ae318c4c2e4c1e38560
zmeyer44 <meyerz@wustl.edu>
2020-06-17 16:42:30 -0400

    hjvb

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ActivityList from './components/ActivityList';
+import Details from './components/Details';
+import Default from './components/Default';
+import Home from './components/Home';
+import VirtualFieldtrip from './components/VirtualFieldtrip';
+import ScrollToTop from "./ScrollToTop";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/activities" component={ActivityList} />
+          <Route path="/details" component={Details} />
+          <Route path="/vft" component={VirtualFieldtrip} />
+          <Route path=" vft" component={VirtualFieldtrip} />
+          <Route component={Default} />
+        </Switch>
+        <ScrollToTop />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Details.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { ActivityConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer2 } from "./Button";
+import { ButtonContainer3 } from "./Button";
+export default class Details extends Component {
+  render() {
+    return (
+      <ActivityConsumer>
+        {(value) => {
+          const { id, img, info, grades, title } = value.detailActivity;
+          return (
+            <div className="container py-5">
+              {/*title}*/}
+              <div className="row">
+                <div className="col-10 mx-auto text-center my-5">
+                  <h1>Project #{id}</h1>
+                </div>
+              </div>
+              {/* end title*/}
+              {}
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <img src={img} className="img-fluid" alt="product"></img>
+                </div>
+                {}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>{title}</h2>
+                  <h4 className="">
+                   <span>Grades: </span> 
+                       {grades}
+                    
+                  </h4>
+                  <h5 className="text-capitalize font-weight-bold mt-4 mb-1">
+                    Summary:
+                  </h5>
+                  <p className="text-muted lead">{info}</p>
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer2>
+                        back to Activities List
+                      </ButtonContainer2>
+                    </Link>
+                    <ButtonContainer3 className="mx-4">
+                      download PDF
+                    </ButtonContainer3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ActivityConsumer>
+    );
+  }
+}
